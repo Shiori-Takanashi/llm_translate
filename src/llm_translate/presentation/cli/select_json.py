c04fb2713891f4bd7eca0f6904e86bad008cd1c0
Takanashi <nao69alter@gmail.com>
2026-03-05 14:35:50 +0900

    refactor: search_dirnameという明確な変数を導入

--- src/llm_translate/presentation/cli/select_json.py
+++ src/llm_translate/presentation/cli/select_json.py
@@ -10,21 +10,23 @@ from llm_translate.application.select.json_usecase import resolve_choice
 @click.command(name="select-json")
 @click.pass_obj
 def select_json(config: AppConfig) -> None:
-    dir_name = config.json_dir
     base_path = config.base_path
+    data_dir = config.data_dir
+    json_dir = config.json_dir
 
+    search_dirname: str = f"{data_dir}/{json_dir}"
     # listup_jsonの返り値をココから変更
     # filesはフルパスであるべき？
-    files = listup_json(base_path, dir_name)
+    files = listup_json(base_path, search_dirname)
 
     click.echo("\n翻訳対象のファイルを選択せよ:")
     for i, p in enumerate(files, 1):
-        click.echo(f"{i}: {convert_path_to_display(p, base_path, dir_name)}")
+        click.echo(f"{i}: {convert_path_to_display(p, base_path, search_dirname)}")
 
     choice = click.prompt("番号", type=int)
 
     selected = resolve_choice(files, choice)
 
     click.secho(
-        f"選択されたファイル: {convert_path_to_display(selected, base_path, dir_name)}"
+        f"選択されたファイル: {convert_path_to_display(selected, base_path, search_dirname)}"
     )
